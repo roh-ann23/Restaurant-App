f2bf2fe839c3d4e7781d500b422090b7327fc8d4
roh-ann23 <158121284+roh-ann23@users.noreply.github.com>
2024-04-12 16:16:51 +0530

    second version of project

--- features/routes/personRoutes.js
+++ features/routes/personRoutes.js
@@ -1,5 +1,6 @@
 
 import express from 'express';
+// this are controller files which we are importing
 import {postPerson,getPerson,personByWorkType,updatePersonById,deletePersonById } from '../controllers/person.controller.js';
 
 const router = express.Router();
